d7a63fcf2c9107764e9ea45444a1868ef2940ba1
Leandro Parice <contato@leandroparice.com.br>
2024-10-07 11:49:28 -0300

    ajuste de estilos globais e fontes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/global';
+
 export function App() {
-	return <h1>Ignite Timer</h1>;
+	return (
+		<ThemeProvider theme={defaultTheme}>
+			<h1>Ignite Timer</h1>
+			<GlobalStyle />
+		</ThemeProvider>
+	);
 }
